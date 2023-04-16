2645d5eb6eff7cb3855dc10cd2702ccdd7e666c1
tooreagen <tooreagen@gmail.com>
2023-04-16 16:54:22 +0300

    final

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,9 +6,7 @@ const HomePage = () => {
       <Description>
         This application is designed to save contacts of phone numbers. It uses
         user registration and authorization. The contact book has the ability to
-        add and delete contacts, as well as a filter on the list of contacts. If
-        you are a registered user, <a href="/login">sign in</a> , if you are a
-        new user, <a href="/register">sign up</a>.
+        add and delete contacts, as well as a filter on the list of contacts.
       </Description>
     </main>
   );
